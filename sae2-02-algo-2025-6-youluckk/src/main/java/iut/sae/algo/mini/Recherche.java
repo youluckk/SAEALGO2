0a89f00d45af01f9dced3dd58b8f9d1b30cba0e7
youluckk <galaad.veron@gmail.com>
2025-06-20 01:15:40 +0200

    dd

--- sae2-02-algo-2025-6-youluckk/src/main/java/iut/sae/algo/mini/Recherche.java
+++
@@ -1,8 +0,0 @@
-package iut.sae.algo.mini;
-
-public class Recherche {
-    public static int chercheMot(String botteDeFoin, String aiguille) {
-        //TODO: A compléter
-        return 0;
-    }
-}
--- sae2-02-algo-2025-6-youluckk/src/main/java/iut/sae/algo/sobriete/Recherche.java
+++
@@ -1,8 +0,0 @@
-package iut.sae.algo.sobriete;
-
-public class Recherche {
-    public static int chercheMot(String botteDeFoin, String aiguille) {
-        //TODO: A compléter
-        return 0;
-    }
-}
